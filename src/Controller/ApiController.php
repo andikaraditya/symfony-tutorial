a8b3f784892691943d22d8d5d6a110be5fe027a6
Andika <andikaraditya1@gmail.com>
2026-06-06 11:34:56 +0700

    add post request with body

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,18 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ApiController extends AbstractController
 {
-    #[Route('/hello', name: 'hello_api')]
+    #[Route('/hello', methods: ['GET'], name: 'hello_api')]
     public function hello(): Response
     {
         return new Response('Hello World');
     }
 
-    #[Route('/users', name: 'users_api')]
+    #[Route('/users', methods: ['GET'], name: 'users_api')]
     public function users(): Response
     {
         return new JsonResponse([
@@ -30,6 +31,20 @@ final class ApiController extends AbstractController
         return new JsonResponse([
             'id' => $id,
             'name' => 'John'
+        ]);
+    }
+
+    #[Route('/users', methods: ['POST'])]
+    public function createUser(Request $request): JsonResponse
+    {
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+
+        return new JsonResponse([
+            'message' => 'User created',
+            'data' => $data
         ]);
     }
 }
