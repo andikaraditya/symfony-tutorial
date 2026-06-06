ee11a1a9d857b6aacb00ff3badeda244864bf7e6
Andika <andikaraditya1@gmail.com>
2026-06-06 11:30:22 +0700

    api json response

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,14 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ApiController extends AbstractController
 {
-    #[Route('/hello', name: 'app_api')]
+    #[Route('/hello', name: 'hello_api')]
     public function hello(): Response
     {
         return new Response('Hello World');
+    }
+
+    #[Route('/users', name: 'users_api')]
+    public function users(): Response
+    {
+        return new JsonResponse([
+            ['id' => 1, 'name' => 'John Doe'],
+            ['id' => 2, 'name' => 'Jane Doe'],
+        ]);
     }
 }
